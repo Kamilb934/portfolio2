1b42b4a7939ab9a11badf1cff0b31e3969226ebd
Kamilb934 <kamilb934@gmail.com>
2019-06-25 23:41:51 +0200

    rewizja responsywna

---
+++ index.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <script src="https://kit.fontawesome.com/aa23600508.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=B612+Mono|Cuprum|Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/fontello/css/fontello.css">
+
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+
+    <title>Kamil Buczyński</title>
+</head>
+
+<body>
+    <section class="wrapper">
+        <h1>
+            <div id="imie">//Kamil</div>
+            <div id="nazwisko">Buczyński</div>
+            <div class="blink">_</div>
+            <div id="clear"></div>
+        </h1>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="container">
+                        <div class="row no-gutters">
+                            <div class="col-md-6">
+
+                               <a href="skills.php"> <figure class="icon-big" id="skills2">
+                                   <i class="icon-cog-alt"></i><br />Skills
+                                </figure></a>
+
+                            </div>
+                            <div class=" col-md-6">
+                                <a href="contact.php"><figure class="icon-big" id="contact2">
+                                    
+                                        <div class="icon" id="contact"><i class="icon-mail"></i><br />Contact</div>
+                                   
+
+                                </figure>
+                                 </a>
+                            </div>
+                            <div class="col-md-6">
+                               <a href="projects.php">
+                                    <figure class="icon-big" id="projects2">
+                                    <i class="icon-terminal"></i><br />Projects
+
+                                </figure>
+                                </a>
+                            </div>
+                            <div class="col-md-6">
+                                
+                                   <a href="cv.php">
+                                   <figure class="icon-big" id="cv2">
+                                    <i class="icon-doc-text-inv"></i><br />CV
+                                </figure>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="col-md-4 text-center" >
+
+                    <div class="quote">The human spirit must prevail over technology.</div>
+                        <div class="containers">
+                            <div class="row no-gutters">
+                                <div class="col-md-4 ">
+                                      <a target="_blank" href="https://github.com/Kamilb934">
+                                     <figure class="icon-small" id="github">
+                                      
+                                            <i class="icon-github-circled"></i>
+                                            Github
+                                    </figure>
+                                    </a>
+                                </div>
+                                <div class=" col-md-4 ">
+                                   <a target="_blank" href="https://linkedin.com/in/kamil-buczyński-web-developer">
+                                    <figure class="icon-small" id="linkedin">
+                                        
+                                            <i class="icon-linkedin-rect-1"></i>
+                                            Linkedin
+                                      
+                                    </figure>
+                                      </a>
+                                </div>
+                                <div class="col-md-4">
+                                    <a target="_blank" href="https://stackoverflow.com/users/11688415/kamilb934">
+                                    <figure class="icon-small" id="stackoverflow">
+                                       <i class="icon-stackoverflow"></i><br />Stack<br/>Overflow
+                                    </figure>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+              
+
+                </div>
+            </div>
+        </div>
+
+
+    </section>
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
